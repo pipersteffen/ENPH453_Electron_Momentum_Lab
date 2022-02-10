28a82cb8741c79650c7a6b76b9d00d9dcb61f9e8
Piper Steffen <piper.steffen@gmail.com>
2022-02-10 15:04:55 -0500

    initial visualization of data

--- experimentalAnalysis.py
+++ experimentalAnalysis.py
@@ -4,13 +4,24 @@ import random
 import math as m
 import csv
 
+STEPSIZE = 0.0635 # cm
+
 data = np.genfromtxt('realData.csv', delimiter=',')
 print(data.shape)
 
 step = data[:,0]
 time = data[:,1]
 position = data[:,2]
-counter_t = data[:,3]
-counter_c = data[:,4]
+counter_total = data[:,3]
+counter_coinc = data[:,4]
 
+counts = counter_coinc/counter_total
 
+# Plot
+# y value: counter_c / counter_total --> gaussian fit
+# x value: postition * 0.5 * step size
+    # in sim we get a coincidence hit every time we send a positron
+
+plt.figure()
+plt.hist(counter_coinc**2/counter_total, bins=51)
+plt.show()
