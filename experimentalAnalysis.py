ac2dc44fce6f421bb19f376b25dbeb1032ecd018
Piper Steffen <piper.steffen@gmail.com>
2022-02-10 14:51:28 -0500

    read csv data into np arrays

--- experimentalAnalysis.py
+++ experimentalAnalysis.py
@@ -4,5 +4,13 @@ import random
 import math as m
 import csv
 
+data = np.genfromtxt('realData.csv', delimiter=',')
+print(data.shape)
+
+step = data[:,0]
+time = data[:,1]
+position = data[:,2]
+counter_t = data[:,3]
+counter_c = data[:,4]
 
 
